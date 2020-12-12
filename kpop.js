0a09aea63d7e5749dadc87b7f56c3d36d8539a7d
18anguyen9 <59636409+18anguyen9@users.noreply.github.com>
2020-12-12 14:57:22 -0800

    Update kpop.js
    fix pie

--- kpop.js
+++ kpop.js
@@ -37,64 +37,34 @@ function makepie(){
                 name: 'BLACKPINK',
                 y: 9744232852,
                 sliced: true,
-                selected: true
+                selected: true,
+                test:'BLACKPINK'
             }, {
                 name: 'TWICE',
-                y: 5175671174
-            }, {
-                name: 'MOMOLAND',
-                y: 681336381
-            }, {
+                y: 5175671174,
+                test:'TWICE'
+            },{
                 name: 'Red Velvet',
-                y: 1667378471
-            }, {
-                name: 'ITZY',
-                y: 791646988
-            }, {
-                name: 'NiziU',
-                y: 187044395
-            }, {
-                name: 'EVERGLOW',
-                y: 458092930
-            }, {
-                name: '4MINUTE',
-                y: 143170458
-            }, {
-                name: 'IZ*ONE',
-                y: 212135214
+                y: 1667378471,
+                test:'Red Velvet'
             },{
                 name: 'Girls\' Generation',
-                y: 1615457160
-            },{
-                name: 'f(x)',
-                y: 131258889	
-            },{
-                name: 'SISTAR',
-                y: 309786464
-            },{
-                name: 'Wonder Girls',
-                y: 96420798
-            },{
-                name: 'MAMAMOO',
-                y: 458964386
-            },{
+                y: 1615457160,
+                test: 'Girls\' Generation'
+            }, {
                 name: '2NE1',
-                y: 1240451348
+                y: 1240451348,
+                test:'2NE1'
             },{
-                name: 'miss A',
-                y: 92201665
+                name: 'ITZY' ,
+                y:791646988
             },{
-                name: 'AOA',
-                y: 85026996
+                name:'MOMOLAND',
+                y:681336381
             },{
-                name: 'Apink',
-                y: 155918645
-            },{
-                name: 'EXID',
-                y: 182512463
-            },{
-                name: '(G)I-DLE',
-                y: 528640395
+                name: 'Other',
+                y: 458964386+528640395+187044395+458092930+143170458+212135214+131258889+309786464+96420798+458964386+85026996+155918645+182512463,
+                test:'MAMAMOO GIDLE NiziU EVERGLOW 4MINUTE IZ*ONE f(x) SISTAR WonderGirls  missA AOA Apink EXID '
             }]
         }]
     });
